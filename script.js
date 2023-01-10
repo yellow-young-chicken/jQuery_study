6a0275e265692127d99096841fbdf64f8e7e5d30
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-01-10 12:39:34 +0000

    done 3

--- script.js
+++ script.js
@@ -280,3 +280,85 @@ $(function() {
     }
   });
 });
+
+
+$(function(){
+  
+  $('.signup-show').click(function(){
+    $('#signup-modal').fadeIn();
+  });
+  
+  $('#close-modal').click(function(){
+    $('#signup-modal').fadeOut();
+  });
+  
+});
+
+
+
+$(function() {
+
+  // 新規登録モーダル
+  
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+  
+  // 言語一覧
+  
+  $('.lesson').hover(
+    function(){
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function(){
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+    );
+
+});
+
+
+$(function() {
+
+  // 新規登録モーダル
+
+  $('.signup-show').click(function() {
+    $('#signup-modal').fadeIn();
+  });
+
+  $('#close-modal').click(function() {
+    $('#signup-modal').fadeOut();
+  });
+
+  // 言語一覧
+
+  $('.lesson').hover(
+    function() {
+      $(this).find('.text-contents').addClass('text-active');
+    },
+    function() {
+      $(this).find('.text-contents').removeClass('text-active');
+    }
+  );
+  
+  $('.faq-list-item').click(function(){
+    var $answer = $(this).find('.answer');
+    if($answer.hasClass('open')){
+      $answer.removeClass('open');
+      $answer.slideUp();
+      
+      $(this).find('span').text('+');
+      
+    } else {
+      $answer.addClass('open');
+      $answer.slideDown();
+      
+      $(this).find('span').text('-');
+    }
+  });
+
+});
